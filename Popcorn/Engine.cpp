959ccd769822c4b9b38e2488a25f7fd82d773dd5
User <User@PUN-PC>
2023-08-10 22:41:01 +0300

    Prototype 8 draw the frame of the level the void Draw_Border function creates and draws border elements the void Draw_Bounds function draws the level frame

--- Popcorn/Engine.cpp
+++ Popcorn/Engine.cpp
@@ -18,8 +18,8 @@ enum EBrick_Type
 };
 
 HWND Hwnd;
-HPEN Highlight_Pen, Letter_Pen, BG_Pen, Brick_Red_Pen, Brick_Blue_Pen, Platform_Circle_Pen, Platform_Inner_Pen, Ball_Pen;
-HBRUSH BG_Brush, Brick_Red_Brush, Brick_Blue_Brush, Platform_Circle_Brush, Platform_Inner_Brush, Ball_Brush;
+HPEN Highlight_Pen, Letter_Pen, BG_Pen, Brick_Red_Pen, Brick_Blue_Pen, Platform_Circle_Pen, Platform_Inner_Pen, Ball_Pen, Border_Blue_Pen, Border_White_Pen;
+HBRUSH BG_Brush, Brick_Red_Brush, Brick_Blue_Brush, Platform_Circle_Brush, Platform_Inner_Brush, Ball_Brush, Border_Blue_Brush, Border_White_Brush;
 
 const int Global_Scale = 3;
 const int Brick_Width = 15;
@@ -68,7 +68,7 @@ char Level_01[Level_Width][Level_Height] =
 };
 
 //------------------------------------------------------------------------------------------------------------
-void Create_Pen_Brush(unsigned char r, unsigned char g, unsigned char b, HPEN &pen, HBRUSH &brush)
+void Create_Pen_Brush(unsigned char r, unsigned char g, unsigned char b, HPEN& pen, HBRUSH& brush)
 {
 	pen = CreatePen(PS_SOLID, 0, RGB(r, g, b));
 	brush = CreateSolidBrush(RGB(r, g, b));
@@ -101,6 +101,9 @@ void Init_Engine(HWND hwnd)
 	Create_Pen_Brush(151, 0, 0, Platform_Circle_Pen, Platform_Circle_Brush);
 	Create_Pen_Brush(0, 128, 192, Platform_Inner_Pen, Platform_Inner_Brush);
 	Create_Pen_Brush(255, 255, 255, Ball_Pen, Ball_Brush);
+	Create_Pen_Brush(85, 255, 255, Border_Blue_Pen, Border_Blue_Brush);
+	Create_Pen_Brush(255, 255, 255, Border_White_Pen, Border_White_Brush);
+
 
 	Level_Rect.left = Level_X_Offset * Global_Scale;
 	Level_Rect.top = Level_Y_Offset * Global_Scale;
@@ -231,7 +234,7 @@ void Draw_Brick_Letter(HDC hdc, int x, int y, EBrick_Type brick_type, ELetter_Ty
 		xform.eDy = (float)y + (float)(brick_half_height);
 		GetWorldTransform(hdc, &old_xform);
 		SetWorldTransform(hdc, &xform);
-	
+
 		// Выводим фон
 		SelectObject(hdc, back_pen);
 		SelectObject(hdc, back_brush);
@@ -297,7 +300,7 @@ void Draw_Platform(HDC hdc, int x, int y)
 	RoundRect(hdc, (x + 4) * Global_Scale, (y + 1) * Global_Scale, (x + 4 + Inner_Width - 1) * Global_Scale, (y + 1 + 5) * Global_Scale, 3 * Global_Scale, 3 * Global_Scale);
 }
 //------------------------------------------------------------------------------------------------------------
-void Draw_Ball(HDC hdc, RECT &paint_area)
+void Draw_Ball(HDC hdc, RECT& paint_area)
 {
 	// 1. Очищаем фон
 	SelectObject(hdc, BG_Pen);
@@ -312,15 +315,62 @@ void Draw_Ball(HDC hdc, RECT &paint_area)
 	Ellipse(hdc, Ball_Rect.left, Ball_Rect.top, Ball_Rect.right - 1, Ball_Rect.bottom - 1);
 }
 //------------------------------------------------------------------------------------------------------------
-void Draw_Frame(HDC hdc, RECT &paint_area)
+void Draw_Border(HDC hdc, int x, int y, bool top_border)
+{// Рисует рамку уровня
+
+	// Основная линия
+	SelectObject(hdc, Border_Blue_Pen);
+	SelectObject(hdc, Border_Blue_Brush);
+
+	if (top_border)
+		Rectangle(hdc, x * Global_Scale, y * Global_Scale, (x + 4) * Global_Scale, (y + 4) * Global_Scale);
+	else
+		Rectangle(hdc, (x + 1) * Global_Scale, y * Global_Scale, (x + 4) * Global_Scale, (y + 4) * Global_Scale);
+
+	// Белая кайма
+	SelectObject(hdc, Border_White_Pen);
+	SelectObject(hdc, Border_White_Brush);
+
+	if (top_border)
+		Rectangle(hdc, x * Global_Scale, y * Global_Scale, (x + 4) * Global_Scale, (y + 1) * Global_Scale);
+	else
+		Rectangle(hdc, x * Global_Scale, y * Global_Scale, (x + 1) * Global_Scale, (y + 4) * Global_Scale);
+
+	// Перфарация
+	SelectObject(hdc, BG_Pen);
+	SelectObject(hdc, BG_Brush);
+
+	if (top_border)
+		Rectangle(hdc, (x + 2) * Global_Scale, (y + 2) * Global_Scale, (x + 3) * Global_Scale, (y + 3) * Global_Scale);
+	else
+		Rectangle(hdc, (x + 2) * Global_Scale, (y + 1) * Global_Scale, (x + 3) * Global_Scale, (y + 2) * Global_Scale);
+}
+//------------------------------------------------------------------------------------------------------------
+void Draw_Bounds(HDC hdc, RECT& paint_area)
+{
+	// Линия слева
+	for (int i = 0; i < 50; i++)
+		Draw_Border(hdc, 2, 1 + i * 4, false);
+
+	// Линия справа
+	for (int i = 0; i < 50; i++)
+		Draw_Border(hdc, 201, 1 + i * 4, false);
+
+	// Линия сверху
+	for (int i = 0; i < 50; i++)
+		Draw_Border(hdc, 3 + i * 4, 0, true);
+
+}
+//------------------------------------------------------------------------------------------------------------
+void Draw_Frame(HDC hdc, RECT& paint_area)
 {// Отрисовка экрана игры
 
 	RECT intersection_rect;
 
-	if (IntersectRect(&intersection_rect, &paint_area, &Level_Rect) )
+	if (IntersectRect(&intersection_rect, &paint_area, &Level_Rect))
 		Draw_Level(hdc);
 
-	if (IntersectRect(&intersection_rect, &paint_area, &Platform_Rect) )
+	if (IntersectRect(&intersection_rect, &paint_area, &Platform_Rect))
 		Draw_Platform(hdc, Level_X_Offset + Platform_X_Pos, Platform_Y_Pos);
 
 	//int i;
@@ -331,8 +381,10 @@ void Draw_Frame(HDC hdc, RECT &paint_area)
 	//	Draw_Brick_Letter(hdc, 20 + i * Cell_Width * Global_Scale, 130, EBT_Red, ELT_O, i);
 	//}
 
-	if (IntersectRect(&intersection_rect, &paint_area, &Ball_Rect) )
+	if (IntersectRect(&intersection_rect, &paint_area, &Ball_Rect))
 		Draw_Ball(hdc, paint_area);
+
+	Draw_Bounds(hdc, paint_area);
 }
 //------------------------------------------------------------------------------------------------------------
 int On_Key_Down(EKey_Type key_type)
@@ -362,8 +414,8 @@ void Move_Ball()
 
 	Prev_Ball_Rect = Ball_Rect;
 
-	next_x_pos = Ball_X_Pos + (int)(Ball_Speed * cos(Ball_Direction) );
-	next_y_pos = Ball_Y_Pos - (int)(Ball_Speed * sin(Ball_Direction) );
+	next_x_pos = Ball_X_Pos + (int)(Ball_Speed * cos(Ball_Direction));
+	next_y_pos = Ball_Y_Pos - (int)(Ball_Speed * sin(Ball_Direction));
 
 	// Корректируем позицию при отражении
 	if (next_x_pos < 0)
@@ -386,7 +438,7 @@ void Move_Ball()
 
 	if (next_y_pos > Max_Y_Pos)
 	{
-		next_y_pos = Max_Y_Pos- Level_Y_Offset - (next_y_pos - Max_Y_Pos);
+		next_y_pos = Max_Y_Pos - Level_Y_Offset - (next_y_pos - Max_Y_Pos);
 		Ball_Direction = M_PI + (M_PI - Ball_Direction);
 	}
 
